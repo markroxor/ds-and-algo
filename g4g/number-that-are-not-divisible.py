a827445ff7226d6df6182be1c35e9ca3ec3803d2
Mohit Rathore <mrmohitrathoremr@gmail.com>
2017-11-28 17:37:45 +0530

    ...

---
+++ g4g/number-that-are-not-divisible.py
@@ -0,0 +1,46 @@
+class readfile:
+    def __init__(self, flag=0):
+        self.flag=flag
+
+    def __iter__(self):
+        if self.flag:
+            f = open("input.txt")
+            for line in f:
+                for n in line.split():
+                    #print type(n),n
+                    yield int(n)
+        else:
+            line = raw_input()
+            for n in line.split():
+                #print type(n),n
+                yield int(n)
+
+def ugly(n):
+    two,three,five=1,1,1
+    ans = 1
+    for i in range(n):
+      
+      if(2*two<=min(3*three,5*five)):
+        ans=two*2
+      elif(3*three<=min(2*two,5*five)):
+        ans=three*3
+      elif(5*five<=min(3*three,2*two)):
+        ans=five*5
+
+      if ans==two*2:
+        two+=1
+      if ans==three*3:
+        three+=1
+      if ans==five*5:
+        five+=1
+      print i+1,ans
+    print (ans)
+
+def main():
+  f = iter(readfile())
+  t = next(f)
+  while t:
+    t-=1
+    n = next(f)
+    ugly(n-1)
+main()
